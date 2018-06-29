51b918feb8d603b4ebd78da560970468999544ca
FIFOFridge <lazydroid97@gmail.com>
2018-06-29 13:49:13 +0200

    added less task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 import gulp from 'gulp'
 import less from 'gulp-less'
 import path from 'path'
+
+gulp.task('less', function () {
+    return gulp.src('./src/renderer/less/*.less')
+      .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+      .pipe(gulp.dest('./static/css'))
+  });
