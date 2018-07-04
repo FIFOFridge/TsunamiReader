477c34faba889898e773d865fbce29498b286ff8
FIFOFridge <lazydroid97@gmail.com>
2018-07-04 03:27:40 +0200

    implemented part of shelf grid

---
+++ src/renderer/components/ShelfView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="window-content-container">
+
+    </div>
+</template>
+
+<style>
+@import "/static/lib/lazygrid.css";
+@import "/static/css/shelf.css";
+</style>
+
+<script>
+export default {
+    
+}
+</script>
+
