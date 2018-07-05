afa1819b12a9234a4d34c796c1105c09a6a6dfa7
FIFOFridge <lazydroid97@gmail.com>
2018-07-05 22:48:09 +0200

    added models cache option

--- src/modules/dataModels.js
+++ src/modules/dataModels.js
@@ -9,11 +9,35 @@ let con = exconsole(logger, console)
 class DataModels {
     constructor() {
         this.modelDirectory = path.join(__static, `models`)
+        this.cachedModels = {}
     }
 
-    makeModel(pathModel) {
+    _containsCachedModel(path) {
+        if(this.cachedModels.hasDefinedProperty(path) &&
+            this.cachedModels.path !== null &&
+            this.cachedModels.path !== undefined) {
+            
+            return true
+        }
+
+        return false
+    }
+
+    _cacheModel(path, value, override) {
+        if(this._containsCachedModel(path) && !(override)) {
+            con.error(path + " is already cached. If you want to update it set overrideCache to true")
+            return
+        }
+
+        this.cachedModels.path = value
+    }
+
+    makeModel(pathModel, cache = false, overrideCache = false) {
         pathModel = path.join(this.modelDirectory, pathModel + '.json')
         
+        if(cache && this._containsCachedModel(pathModel) && !(overrideCache))
+            return this.cachedModels.pathModel 
+
         con.debug('creating object from file: ' + pathModel)
         
         if(!(fs.existsSync(pathModel))) {
@@ -22,8 +46,13 @@ class DataModels {
         }
 
         var content = fs.readFileSync(pathModel, "utf8")
+        var model = JSON.parse(content)
 
-        return JSON.parse(content)
+        if(cache) {
+            this._cacheModel(pathModel, false, overrideCache)
+        }
+
+        return model
     }
 
     fromFile(pathModel) {
