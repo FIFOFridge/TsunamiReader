b0d2ba94a9c3ae1869a5960d08d8cb20f803c6cb
FIFOFridge <lazydroid97@gmail.com>
2018-07-09 02:09:33 +0200

    added logs

--- src/modules/bookManager.js
+++ src/modules/bookManager.js
@@ -15,9 +15,11 @@ class BookManager {
         this.bookCollection = {}
 
         if (fs.existsSync(this.booksPath)) {
+            con.info(`reading books from local file: ${this.booksPath}`)
             var booksContent = fs.readFileSync(this.booksPath, 'UTF8')
             this.bookCollection = JSON.parse(booksContent)
         } else {
+            con.info(`default file not found: ${this.booksPath}, writing new one`)
             fs.writeFileSync(this.booksPath, JSON.stringify(this.bookCollection))
         }
     }
@@ -89,6 +91,7 @@ class BookManager {
     }
 
     save() {
+        con.info(`saving ${this.booksPath}`)
         fs.writeFileSync(this.booksPath, JSON.stringify(this.bookCollection))
     }
 
