bc3f3fc0777082ab2a014b22c320cd7a54510e06
FIFOFridge <lazydroid97@gmail.com>
2018-07-10 12:40:31 +0200

    updated windowRouter

--- src/modules/windowRouter.js
+++ src/modules/windowRouter.js
@@ -1,9 +1,28 @@
-import fs from 'fs';
-import { remote } from 'electron';
+import util from 'util'
+import exconsole from './helpers/loggerConsole'
+import logger from './helpers/logger'
+
+let con = exconsole(logger, console)
 
 class WindowRouter {
-    beforeEach(to, from, next) { //for global navigation processing
-        console.log("[ROUTING] from: " + from + " next: " + next);
+    constructor(fnBeforeEach, owner, ownerName) {
+        if(!(util.isFunction(fnBeforeEach))) {
+            con.error("fnBeforeEach isn't function")
+            throw TypeError("fnBeforeEach isn't function")
+        }
+
+        if(!(util.isString(ownerName))) {
+            con.error("ownerName isn't string")
+            throw TypeError("ownerName isn't string")
+        }
+
+        this.owner = owner
+        this.fnBeforeEach = fnBeforeEach
+    }
+
+    beforeEach(to, from, next) {
+        con.info(`routing ${this.ownerName} from: ${from} to: ${to}`)
+        this.fnBeforeEach(to, from, next, owner)
     }
 }
 
