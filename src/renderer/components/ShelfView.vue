e0d2c7cae0248342b4c1abc276865e09b0dca519
FIFOFridge <lazydroid97@gmail.com>
2018-07-12 00:20:21 +0200

    separated app tiles grid and book tiles grid

--- src/renderer/components/ShelfView.vue
+++ src/renderer/components/ShelfView.vue
@@ -2,7 +2,9 @@
     <div>
         <app-titlebar></app-titlebar>
         <div class="window-content-container">
-            <shelf-grid style="width=100%; height: 100%;">
+            <shelf-grid id="app-tiles-grid" style="width=100%; height: 100%;">
+            </shelf-grid>
+            <shelf-grid id="books-tiles-grid">
             </shelf-grid>
         </div>
     </div>
