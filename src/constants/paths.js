79284c8e781b8c16d6cbe607a60b7c32d72c870e
FIFOFridge <lazydroid97@gmail.com>
2018-08-01 14:05:59 +0200

    fixed directories paths

--- src/constants/paths.js
+++ src/constants/paths.js
@@ -4,8 +4,8 @@ import fs from 'fs'
 const userDataPath = app.getPath('userData')
 
 export default {
-    appStorage: fs.join(userDataPath, '/TsunamiReader'),
-    extractedEpubs: fs.join(appStorage, '/extracted'),
-    settingsDirectory: fs.join(appStorage, '/settings'),
-    settingsPath: fs.join(settingsDirectory, 'appSettings.json')
+    appStorage: fs.join(userDataPath, '/TsunamiReader/'),
+    extractedEpubs: fs.join(appStorage, '/extracted/'),
+    settingsDirectory: fs.join(appStorage, '/settings/'),
+    settingsPath: fs.join(settingsDirectory, '/appSettings.json')
 }
