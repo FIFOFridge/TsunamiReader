6e9f1c22045331230285d722c04c50c77a49865c
FIFOFridge <lazydroid97@gmail.com>
2018-08-26 19:17:55 +0200

    improved app grid layout, buttons replaced by clickable grid tiles

--- src/renderer/components/ShelfView/ShelfGridItem.vue
+++ src/renderer/components/ShelfView/ShelfGridItem.vue
@@ -1,109 +1,101 @@
 <template>
-    <div :id="[id]" class="grid-item" :class="[priority, type]">
-        <!-- book -->
-        <div v-if="this.bookObject ==! null && this.bookObject ==! undefined"
-            class="book" :style="[backgroundImage]"
-        >
-            <div class="book-title">{{ book.title }}</div>
-        </div>
-        <!-- app -->
-        <!-- <div class="app" v-else>for manual setup by external style sheet -->
-        <div class="img-container">
-            <img v-if="!this.isSVG" class="_img" :src="this.img"/>
-            <svg v-else class="_img _svg" viewBox="0 0 24 24">
-                <path :fill="this.svgFill" :d="this.img"/>
-            </svg>
-        </div>
-
-        
-            <!-- <router-link :to="this.defaultActionLink" tag="button">{{this.label}}</router-link> -->
+        <div :id="[id]" class="grid-item" :class="[priority, type]">
             <router-link :to="{path: this.defaultActionLink}">
-                <button class="app-button">{{this.label}}</button>
+                <!-- book -->
+                <div v-if="this.bookObject ==! null && this.bookObject ==! undefined"
+                    class="book" :style="[backgroundImage]"
+                >
+                    <div class="book-title">{{ book.title }}</div>
+                </div>
+                <!-- app -->
+                <!-- <div class="app" v-else>for manual setup by external style sheet -->
+                <div class="img-container">
+                    <img v-if="!this.isSVG" class="_img" :src="this.img"/>
+                    <svg v-else class="_img _svg" viewBox="0 0 24 24">
+                        <path :fill="this.svgFill" :d="this.img"/>
+                    </svg>
+                </div>
             </router-link>
-        <!-- </div> -->
-    </div>
+        </div>
+    
 </template>
 
 <style>
 /* app */
 .grid-item.priority-high {
-    order: -2;
+  order: -2;
 }
 
 /* fav books */
 .grid-item.priority-medium {
-    order: -1;
+  order: -1;
 }
 
 /* casual books */
 .grid-item.priority-low {
-    order: 0;
+  order: 0;
 }
 </style>
 
 <script>
 export default {
-    props: {
-        img: "", //encoded as base64 || svg,
-        isSVG: false,
-        svgFill: "#000000",
-        // imgPosition: '',
-        // isBook: true,
-        bookObject: null,
-        link: "",
-        label: "click meh!",
-        id: null
-    },
-    data: function() {
-        return {
-            title: "",
-            isFavourite: false,
-            priority: "",
-            defaultActionLink: "",
-            removeActionLink: "",//only for book
-            backgroundImage: "",
-            type: ""
-        }
-    },
-    //exectue before mount
-    created: function() {
-        //update data
-        if(this.bookObject ==! null && this.bookObject ==! undefined) {
-            this.title = this.bookObject.title
-            this.isFavourite = this.bookObject.isFavourite
+  props: {
+    img: "", //encoded as base64 || svg,
+    isSVG: false,
+    svgFill: "#000000",
+    // imgPosition: '',
+    // isBook: true,
+    bookObject: null,
+    link: "",
+    label: "click meh!",
+    id: null
+  },
+  data: function() {
+    return {
+      title: "",
+      isFavourite: false,
+      priority: "",
+      defaultActionLink: "",
+      removeActionLink: "", //only for book
+      backgroundImage: "",
+      type: ""
+    };
+  },
+  //exectue before mount
+  created: function() {
+    //update data
+    if (this.bookObject == !null && this.bookObject == !undefined) {
+      this.title = this.bookObject.title;
+      this.isFavourite = this.bookObject.isFavourite;
 
-            this.priority = this.isFavourite ? "priority-medium" : "priority-low"
-        } else {
-            this.priority = "priority-high"
-        }
-
-        if(this.bookObject ==! null && this.bookObject ==! undefined) {
-            this.type = "book"
-        } else {
-            this.type = "app"
-        }
-
-        this.makeActionLinks()
-        
-        //format for base64 src
-        if(!(this.isSVG)) {
-            this.img = `data:image/png;base64, ${this.img}`
-        }
-        
-    },
-    methods: {
-        makeActionLinks: function() {
-            if(this.bookObject ==! null && this.bookObject ==! undefined)
-            {
-                this.defaultActionLink = "/action/book/view/" + this.link
-                this.removeActionLink = "/action/book/remove/" + this.link
-            } else {
-                this.defaultActionLink = this.link
-            }
-        }
-    },
-    computed: {
-
+      this.priority = this.isFavourite ? "priority-medium" : "priority-low";
+    } else {
+      this.priority = "priority-high";
     }
-}
+
+    if (this.bookObject == !null && this.bookObject == !undefined) {
+      this.type = "book";
+    } else {
+      this.type = "app";
+    }
+
+    this.makeActionLinks();
+
+    //format for base64 src
+    if (!this.isSVG) {
+      this.img = `data:image/png;base64, ${this.img}`;
+    }
+  },
+  methods: {
+    makeActionLinks: function() {
+      if (this.bookObject == !null && this.bookObject == !undefined) {
+        this.defaultActionLink = "/action/book/view/" + this.link;
+        this.removeActionLink = "/action/book/remove/" + this.link;
+      } else {
+        this.defaultActionLink = this.link;
+      }
+    }
+  },
+  computed: {}
+};
 </script>
