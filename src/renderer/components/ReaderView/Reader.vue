ae6e179c8553f7733c56cd62b4982f994cbe3e11
FIFOFridge <lazydroid97@gmail.com>
2018-10-05 20:09:04 +0200

    Changed navigation controls slots
    - Replaced slots with predefined html structure
    - Added svgs
    - Wrapped with buttons

--- src/renderer/components/ReaderView/Reader.vue
+++ src/renderer/components/ReaderView/Reader.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
     <div class="epub-container">
-      <slot name="prev-btn" :goToPrevPage="goToPrevPage">
-        <div id="prev" class="arrow" @click="goToPrevPage">‹</div>
-      </slot>
+      <button id="prev-btn" @click="goToPrevPage">
+        <svg viewBox="0 0 24 24">
+            <path fill="#000000" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+        </svg>
+        <!-- <div id="prev" class="arrow" @click="goToPrevPage">‹</div> -->
+      </button>
       <slot name="book-content" :ready="ready">
         <div :id="bookArea"></div>
       </slot>
-      <slot name="next-btn" :goToNextPage="goToNextPage">
-        <div id="next" class="arrow" @click="goToNextPage">›</div>
-      </slot>
+      <button id="next-btn" @click="goToNextPage">
+        <svg viewBox="0 0 24 24">
+            <path fill="#000000" d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+        </svg>
+        <!-- <div id="prev" class="arrow" @click="goToPrevPage">‹</div> -->
+      </button>
     </div>
     <!-- <slot name="progress-bar" :onChange="onChange" :ready="ready">
       <div class="epub-reading-progress-bar">
