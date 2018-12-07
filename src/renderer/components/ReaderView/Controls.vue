d6201a2321969eb7d0d20251420057a56517df23
FIFOFridge <FIFOFridge@outlook.com>
2018-12-07 22:16:55 +0100

    changed icon

--- src/renderer/components/ReaderView/Controls.vue
+++ src/renderer/components/ReaderView/Controls.vue
@@ -11,7 +11,8 @@
             <!-- Shelf -->
             <button v-on:click="$emit('clicked-shelf')">
                 <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                    <path d="M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z" />
+                    <path d="M19.5,9V1.5H16.5V9H13.5V1.5H10.5V9H7.5V1.5H4.65V9H3V10.5H21V9H19.5M19.5,13.5H16.5V21H13.5V13.5H10.5V21H7.5V13.5H4.65V21H3V22.5H21V21H19.5V13.5Z" />
+                    <!-- <path d="M13,3V9H21V3M13,21H21V11H13M3,21H11V15H3M3,13H11V3H3V13Z" /> -->
                 </svg>
             </button>
             
