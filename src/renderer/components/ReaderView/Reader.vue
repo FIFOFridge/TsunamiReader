e2736b384aba6e4175edf2f2b871cb573fd9ee6f
FIFOFridge <FIFOFridge@outlook.com>
2018-12-09 15:00:29 +0100

    fixed previous button page change while drawer is open

--- src/renderer/components/ReaderView/Reader.vue
+++ src/renderer/components/ReaderView/Reader.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>
-            <button id="prev-btn" @click="wrapper.previousPage()" :class="{'drawer-mode': isDrawerOpen}">
+            <button id="prev-btn" @click="handlePreviousButton()" :class="{'drawer-mode': isDrawerOpen}">
                 <svg viewBox="0 0 24 24">
                         <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
                 </svg>
@@ -109,6 +109,13 @@ export default {
             if(this.wrapper !== undefined) {
                 this.wrapper.calcRendentionSize(true)
                 this.wrapper.updateRendentionSize(true)
+            }
+        },
+        handlePreviousButton() {
+            if(!this.isDrawerOpen && this.loaded)
+                wrapper.previousPage()
+            else if(this.isDrawerOpen) {
+                //TODO: emit drawer close
             }
         }
     },
