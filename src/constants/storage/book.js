cddad9af3b91b6d4d648f9776d411774071f65d6
FIFOFridge <FIFOFridge@outlook.com>
2019-01-24 23:17:02 +0100

    updated book storage

--- src/constants/storage/book.js
+++ src/constants/storage/book.js
@@ -4,9 +4,11 @@ export default storage.create.predefined.typeRestricted(
     [
         {key: 'url', type: storage.dataTypes.String},
         {key: 'fontSize', type: storage.dataTypes.Number, default: 100},
-        {key: 'backgroundImage', type: storage.dataTypes.String},
         {key: 'flow', type: storage.dataTypes.String, default: 'paginated'},
         {key: 'currentCFI', type: storage.dataTypes.String},
-        {key: 'progress', type: storage.dataTypes.Number, default: 0}
+        {key: 'progress', type: storage.dataTypes.Number, default: 0},
+        {key: 'isLocal', type: storage.dataTypes.Boolean},
+        {key: 'md5', type: storage.dataTypes.String},
+        {key: 'cover', type: storage.dataTypes.String} //base64
     ]
 )
